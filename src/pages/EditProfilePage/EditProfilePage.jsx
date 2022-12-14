55444ef763fa7be714fefe980cec1bef746431af
Antonio Gloria <angloria87@gmail.com>
2022-12-14 14:47:58 -0600

    udpated edit profile page to include tagline and bio fields

--- src/pages/EditProfilePage/EditProfilePage.jsx
+++ src/pages/EditProfilePage/EditProfilePage.jsx
@@ -1,15 +1,17 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
 import userService from "../../services/user.service";
-import { Button, Form, FormGroup, FormLabel, Card } from "react-bootstrap";
+import { Button, Form, FormGroup, FormLabel, Card, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const EditProfilePage = () => {
   const { isLoggedIn, logOutUser, user, setUser } = useContext(AuthContext);
-  const { username, avatar, coverImg } = user;
+  const { username, avatar, coverImg, bio, tagline } = user;
 
   const [avatarUrl, setAvatarUrl] = useState(avatar);
   const [coverUrl, setCoverUrl] = useState(coverImg);
+  const [newTagline, setNewTagline] = useState(tagline);
+  const [newBio, setNewBio] = useState(bio);
 
   const navigate = useNavigate();
 
@@ -40,12 +42,19 @@ const EditProfilePage = () => {
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      await userService.updateOne(username, {avatar:avatarUrl, coverImg: coverUrl});
+      await userService.updateOne(username, {
+        avatar: avatarUrl,
+        coverImg: coverUrl,
+        tagline: newTagline,
+        bio: newBio
+      });
 
       setUser({
         username,
         avatar: avatarUrl,
         coverImg: coverUrl,
+        tagline: newTagline,
+        bio: newBio,
         isLoggedIn,
         logOutUser,
       });
@@ -61,31 +70,62 @@ const EditProfilePage = () => {
     <div className="d-flex align-items-center"
       style={{height: "90vh"}}
     >
-      <Card className="w-50 m-auto text-center shadow">
+      <Card className="w-25 m-auto text-center shadow">
+        <Card.Header>
+          <h2>Edit Profile</h2>
+        </Card.Header>
         <Card.Body>
-        <h1>{username} - Edit Profile</h1>
         <Form onSubmit={handleSubmit} encType="multipart/form-data">
-          <div className="d-flex">
-            <Card className="w-25 p-3">
-              <FormGroup controlId="avatarFile">
-                <FormLabel>Change Avatar</FormLabel>
-                <br/>
-                <img src={avatarUrl} alt="avatar-img" style={{width:"200px", objectFit:"cover"}}/>
-                <FormLabel className="text-muted">200px × 200px</FormLabel>
-                <Form.Control type="file" onChange={handleAvatarUpload}/>
-              </FormGroup>
-            </Card>
+          <Row>
+            <Col className="text-start">
+              <Card>
+                <Card.Body>
+                  <FormGroup controlId="tagline-text" className="mb-3">
+                    <Form.Label>Your Tagline:</Form.Label>
+                    <Form.Control type="text" value={newTagline} onChange={(e) => setNewTagline(e.target.value)}/>
+                  </FormGroup>
+                  <FormGroup controlId="bio-textarea">
+                    <Form.Label>Your Bio:</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={7}
+                      style={{resize:"none"}}
+                      value={newBio}
+                      onChange={(e) => setNewBio(e.target.value)}
+                    />
+                  </FormGroup>
+                </Card.Body>
+              </Card>
+            </Col>
 
-            <Card className="w-75 p-3 d-flex flex-column justify-content-center">
-              <FormGroup controlId="coverFile">
-                <FormLabel>Change Cover Image</FormLabel>
-                <img src={coverUrl} alt="cover-img" style={{width:"100%"}}/>
-                <FormLabel className="text-muted">1920px × 360px</FormLabel>
-                <Form.Control type="file" onChange={handleCoverUpload} className="mt-4"/>
+            <Col>
+              <FormGroup controlId="avatarFile" className="m-1">
+                <Card>
+                  <Card.Header>
+                    <FormLabel>Change Avatar</FormLabel>
+                  </Card.Header>
+                  <Card.Img src={avatarUrl} alt="avatar-img"/>
+                  <Card.Body>
+                    <FormLabel className="text-muted">200px × 200px</FormLabel>
+                    <Form.Control type="file" onChange={handleAvatarUpload}/>
+                  </Card.Body>
+                </Card>
               </FormGroup>
+            </Col>
+          </Row>
+          <FormGroup controlId="coverFile" className="m-1">
+            <Card>
+              <Card.Header>
+                <FormLabel>Change Cover Image</FormLabel>
+              </Card.Header>
+              <Card.Img src={coverUrl} alt="cover-img"/>
+              <Card.Body>
+                <FormLabel className="text-muted">1920px × 360px</FormLabel>
+                <Form.Control type="file" onChange={handleCoverUpload}/>
+              </Card.Body>
             </Card>
-          </div>
-          <Button variant="primary" type="submit">Update Profile</Button>
+          </FormGroup>
+          <Button variant="primary" type="submit" className="mt-2">Update Profile</Button>
         </Form>
         </Card.Body>
       </Card>

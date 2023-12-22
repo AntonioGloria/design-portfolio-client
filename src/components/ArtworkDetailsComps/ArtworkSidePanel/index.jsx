a67d9a9856d589652ba30d7b42d14471f1e6e2ff
Antonio Gloria <angloria87@gmail.com>
2023-12-22 12:34:40 -0600

    Added category and medium info to the ArtworkSidePanel component

--- src/components/ArtworkDetailsComps/ArtworkSidePanel/index.jsx
+++ src/components/ArtworkDetailsComps/ArtworkSidePanel/index.jsx
@@ -1,23 +1,31 @@
 import { Link } from "react-router-dom";
+import { Badge } from "react-bootstrap";
 import moment from "moment";
 
 const ArtworkSidePanel = (props) => {
   const { artData } = props;
-  const { creator, title, description, createdAt } = artData;
+  const { creator, title, description, category, medium, createdAt } = artData;
+
   return (
     <div className="p-3 pt-5">
       <Link
         to={`/${creator.username}`}
         className='text-decoration-none text-reset'
       >
-        <div className='d-flex align-items-center mb-5'>
+        <div className='d-flex align-items-center mb-4'>
           <img src={creator.avatar} alt="" style={{width:"75px", borderRadius:"50%"}}/>
           <h3 className='ms-3'>{creator.username}</h3>
         </div>
       </Link>
       <h3>{title}</h3>
       <em className='text-muted'>Posted on {moment(createdAt).format('MMM DD, YYYY')}</em>
-      <p className='mt-5'>{description}</p>
+      <p className='my-4'>{description}</p>
+      <Link to={`/artworks?category=${category}`} className="me-3">
+        <Badge bg={"secondary"}>{category}</Badge>
+      </Link>
+      <Link to={`/artworks?category=${category}&medium=${medium}`}>
+        <Badge bg={"secondary"}>{medium}</Badge>
+      </Link>
     </div>
   )
 }

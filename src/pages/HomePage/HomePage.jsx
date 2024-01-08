17d364f0a42d6e0a447763540430a86a777b84a1
Antonio Gloria <angloria87@gmail.com>
2024-01-08 11:08:02 -0600

    Homepage now shows all exisiting artworks

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,47 @@
 import "./HomePage.css";
 import JutsuLogo from "../../assets/img/JutsuLogo.svg"
+import { useState, useEffect } from "react";
+import artworkService from "../../services/artwork.service";
+import { Container, Row, Col } from "react-bootstrap";
+import ArtworkThumbnail from "../../components/ArtworkThumbnail";
+import Loading from "../../components/Loading/Loading";
 
 function HomePage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await artworkService.getAll();
+      setArtworks(response.data);
+      setIsLoading(false);
+    }
+    getData();
+  }, []);
+
+  if (isLoading) {
+    return <Loading/>
+  }
+
   return (
-    <header className="text-center">
-      <h1 className="display-1">Welcome to</h1>
-      <img src={JutsuLogo} alt="jutsu-logo"/>
-      <h1 className="text-muted">The Place to Show and Grow Your Craft</h1>
-    </header>
+    <>
+      <header className="text-center">
+        <h1 className="display-1">Welcome to</h1>
+        <img src={JutsuLogo} alt="jutsu-logo"/>
+        <h1 className="text-muted">The Place to Show and Grow Your Craft</h1>
+      </header>
+      <Container>
+        <Row xs={"auto"}>
+          { artworks.map(artwork => {
+            return (
+              <Col key={artwork._id}>
+                <ArtworkThumbnail imageSrc={artwork.assets[0]}/>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+    </>
   );
 }
 
